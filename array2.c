00a33a528994c701eb7c9286f246909caacbda9e
8792002889_PARVAM_CEC <skanda092006@gmail.com>
2024-12-09 14:15:35 +0530

    All Files

--- array2.c
+++ array2.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() 
+int main()
 {
-    int size=0,a[100],i,sum=0;
-    scanf("%d",&size);
-    for(i=0;i<size;scanf("%d",&a[i++]));
-    for(i=0;i<size;printf(" %d ",a[i++]));
-    sum += a[i];
-    printf("sum of all the elements is %d",sum);
-       return 0;
+	int size=0,a[100],i,sum=0;
+	scanf("%d",&size);
+	for( i=0;i<size;scanf("%d",&a[i++]));
+
+    for(i=0;i<size;printf("%d ",a[i]),sum+=a[i++]);
+    printf("Sum of all elements is %d",sum);
+
 }
