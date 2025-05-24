9384d21bd8a4461ea5ef051df4ca40e840e39a4f
8792002889_PARVAM_CEC <skanda092006@gmail.com>
2025-05-24 09:31:35 +0530

    All Files

--- Cpp_traning/fourth.cpp
+++ Cpp_traning/fourth.cpp
@@ -7,15 +7,15 @@ int main() {
     float average;
     std::cout << " NAME: R SKANDA HERLE \n YEAR:2024-28\n BRANCH:ISE\n SEM:2ND\n COLLEGE:CEC";
     std::cout << " \n**************************************************************************************************\n";
-    cout << "Enter marks for subject 1: ";
+    cout << " marks for subject 1: ";
     cin >> m1;
-    cout << "Enter marks for subject 2: ";
+    cout << " marks for subject 2: ";
     cin >> m2;
-    cout << "Enter marks for subject 3: ";
+    cout << " marks for subject 3: ";
     cin >> m3;
-    cout << "Enter marks for subject 4: ";
+    cout << " marks for subject 4: ";
     cin >> m4;
-    cout << "Enter marks for subject 5: ";
+    cout << " marks for subject 5: ";
     cin >> m5;
     total = m1 + m2 + m3 + m4 + m5;
     average = total / 5.0;
@@ -25,15 +25,23 @@ int main() {
     if (average >= 90) 
     {
         cout << "\tGrade = A";
+        cout << "\n RESULT:student have passed";
     } 
     else if (average >=70) 
     {
         cout << "\tGrade = B";
+        cout << "\n RESULT:student have passed";
     } 
-    else 
+    else if (average >=60)
     {
         cout << "\tGrade = C";
+        cout << "\n RESULT:student have passed";
     }
-
+    else
+    {
+        cout << "\tGrade = D";
+        cout << "\nRESULT=student have failed";
+    }
+    
     return 0;
 }
