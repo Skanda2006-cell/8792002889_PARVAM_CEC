3cebead6eda53d8bfeeb88c047c4bbbddac6babf
8792002889_PARVAM_CEC <skanda092006@gmail.com>
2025-05-26 11:04:18 +0530

    All Files

--- Cpp_traning/twentyfifth.cpp
+++ Cpp_traning/twentyfifth.cpp
@@ -1,54 +1,48 @@
 #include <iostream>
 using namespace std;
-int main() 
-{
-    int size;
-    cout << "Enter the size of square matrices: ";
-    cin >> size;
-    int matrix1[size][size], matrix2[size][size], result[size][size];
-    cout << "\nEnter elements of first matrix:\n";
-    for(int i = 0; i < size; i++) 
-    {
-        for(int j = 0; j < size; j++) 
-        {
-            cout << "Enter element [" << i << "][" << j << "]: ";
-            cin >> matrix1[i][j];
+int main()
+ {
+    int r1, c1, r2, c2;
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible!" << endl;
+        return 0;
+    }
+    int mat1[r1][c1], mat2[r2][c2], result[r1][c2];
+    cout << "Enter elements of first matrix:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            cin >> mat1[i][j];
         }
     }
-    cout << "\nEnter elements of second matrix:\n";
-    for(int i = 0; i < size; i++) 
-    {
-        for(int j = 0; j < size; j++) 
-        {
-            cout << "Enter element [" << i << "][" << j << "]: ";
-            cin >> matrix2[i][j];
+    cout << "Enter elements of second matrix:\n";
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            cin >> mat2[i][j];
         }
     }
-    for(int i = 0; i < size; i++) 
-    {
-        for(int j = 0; j < size; j++) 
-        {
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
             result[i][j] = 0;
         }
     }
-    for(int i = 0; i < size; i++) 
-    {
-        for(int j = 0; j < size; j++) 
-        {
-            for(int k = 0; k < size; k++) 
-            {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
             }
         }
     }
-    cout << "\nResult of matrix multiplication:\n";
-    for(int i = 0; i < size; i++) 
-    {
-        for(int j = 0; j < size; j++) 
-        {
-            cout << result[i][j] << "\t";
+    cout << "Result of matrix multiplication:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            cout << result[i][j] << " ";
         }
         cout ;
     }
+
     return 0;
 }
